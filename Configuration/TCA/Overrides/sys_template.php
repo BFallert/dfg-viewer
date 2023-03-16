6d0c672f1c4c700009df86d9246a51ea1d21801f
Bernd Fallert <bernd.fallert@bib.uni-mannheim.de>
2023-03-16 10:56:42 +0100

    Minimal renaming to be able to use this module next to the original DFG viewer

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,19 +3,19 @@ defined('TYPO3_MODE') or die();
 
 // Register static typoscript.
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'dfgviewer',
+    'dfgviewer-ma',
     'Configuration/TypoScript',
-    'DFG-Viewer: Main TypoScript'
+    'DFG-Viewer-ma: Main TypoScript'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'dfgviewer',
+    'dfgviewer-ma',
     'Configuration/TypoScript/Plugins/News/changelog.typoscript',
-    'DFG-Viewer: News: Changelog Configuration'
+    'DFG-Viewer-ma: News: Changelog Configuration'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'dfgviewer',
+    'dfgviewer-ma',
     'Configuration/TypoScript/Plugins/News/faq.typoscript',
-    'DFG-Viewer: News: FAQ Configuration'
+    'DFG-Viewer-ma: News: FAQ Configuration'
 );
